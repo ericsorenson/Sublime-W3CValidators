ab173edc9911c7f5e6cd02b52660f81947ed0b1f
Eric Sorenson <ericsorenson@Core.local>
2013-11-26 10:18:10 -0600

    Alpha Version

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 Sublime-W3CValidators
 =====================
 
-W3C markup validator package for the Sublime Text 2 text editor.
+## About
+
+This is a simplified markup validator package for the Sublime Text 2 text editor that uses the W3C validator web service located at [http://validator.w3.org](http://validator.w3.org).
+
+Unlike similar packages, curl is not required! 
+
+##Installation
+
+Via the [Sublime Package Manager](http://wbond.net/sublime_packages/package_control):
+
+* `Ctrl+Shift+P` or `Cmd+Shift+P` in Linux / Windows / OS X
+* Type `install`, select `Package Control: Install Package`
+* Select `W3CValidators`
+
+##Usage
+
+Open an HTML or SVG file, and from the Tools menu go to W3C Validators, and then select the type of document you're validating. The results will be displayed in an alert box.
+
+##Design Decisions
+
+Automatic detection of document types has proven unrealiable, therefore only explicit document type validation is supported. Not all document types are supported, because most of them should be depricated in practice.
---
+++ W3CValidators.py
@@ -0,0 +1,62 @@
+import sublime
+import sublime_plugin
+import urllib
+import json
+
+class AbstractValidator(sublime_plugin.TextCommand):
+
+  markupValidatorUrl = 'http://validator.w3.org/check'
+  cssValidatorUrl = 'http://jigsaw.w3.org/css-validator/validator'
+
+  def validate(self, format, validatorUrl):
+    region = sublime.Region(0, self.view.size())
+    fileContents = self.view.substr(region)
+    params = { 'fragment':fileContents, 'doctype': format, 'output':'json' }
+    encodedParams = urllib.urlencode(params)
+    output = urllib.urlopen(validatorUrl, encodedParams).read()
+    results = json.loads(output)
+    messageContents = ''
+    if not results['messages']:
+      messageContents = 'This document was successfully checked as %s!' % format
+    else:
+      formattedMessages = []
+      formattedMessages.append('Errors found while checking this document as %s!\n\n' % format)
+      for message in results['messages']:
+        formattedMessages.append('Line %s: %s\n\n' % (message['lastLine'], message['message']))
+      messageContents = messageContents.join(formattedMessages)
+    sublime.message_dialog(messageContents)
+
+class Validatehtml5Command(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('HTML5', self.markupValidatorUrl)
+
+class Validatehtml4strictCommand(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('HTML 4.01 Strict', self.markupValidatorUrl)
+
+class Validatehtml4transitionalCommand(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('HTML 4.01 Transitional', self.markupValidatorUrl)
+
+class Validatesvg11Command(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('SVG 1.1', self.markupValidatorUrl)
+
+class Validatesvg11tinyCommand(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('SVG 1.1 Tiny', self.markupValidatorUrl)
+
+class Validatesvg11basicCommand(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('SVG 1.1 Basic', self.markupValidatorUrl)
+
+class Validatecss3Command(AbstractValidator):
+
+  def run(self, edit):
+    self.validate('css3', self.cssValidatorUrl)
